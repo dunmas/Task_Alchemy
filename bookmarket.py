e44c4979ee7446ee04518683d805d5f47682fcbf
Danila <levafork@gmail.com>
2023-03-08 15:05:16 +0300

    Тестовые данные и их импорт, небольшие правки для самих классов, для удобства - в create_engine сначала стираем предыдущие данные в БД. Данные добавляются корректно.

--- bookmarket.py
+++ bookmarket.py
@@ -44,7 +44,7 @@ class Sale(Base):
     __tablename__ = 'sale'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=False)
+    price = sq.Column(sq.Float, nullable=False)
     date_sale = sq.Column(sq.Date, nullable=False)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"))
     count = sq.Column(sq.Integer, nullable=False)
@@ -53,4 +53,5 @@ class Sale(Base):
 
 
 def create_tables(engine):
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import sqlalchemy
+import json
 
 from sqlalchemy.orm import sessionmaker
-from bookmarket import create_tables
-from password import password
+from bookmarket import create_tables, Publisher, Shop, Book, Stock, Sale
+from user_data import login, password
 
-DSN = f'postgresql://postgres:{password}@localhost:5432/alchemy_db'
+database_system = 'postgresql'
+database = 'alchemy_db'
+
+DSN = f'{database_system}://{login}:{password}@localhost:5432/{database}'
 engine = sqlalchemy.create_engine(DSN)
-
-
 create_tables(engine)
-
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+with open('fixtures/tests_data.json', 'r') as fd:
+    data = json.load(fd)
+
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
+session.commit()
 
 session.close()
